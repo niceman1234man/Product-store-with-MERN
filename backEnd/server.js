069b39070c1e2c7aea537ac3bb5d964e3b5e0527
niceman1234man <yihunietarekegn18@gmail.com>
2024-08-22 17:07:48 +0300

    server

---
+++ backEnd/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { connectDb } from './config/db.js';
+import productRoutes from './routes/product.route.js'
+import cors from 'cors'
+
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use("/products",productRoutes)
+app.listen(5000,()=>{
+    connectDb();
+    console.log("server running on servrr 5000");
+})
