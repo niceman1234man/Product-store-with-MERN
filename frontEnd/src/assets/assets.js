57f090d2dd95d6cfdc7bc365cb5e9afaee0b1776
niceman1234man <yihunietarekegn18@gmail.com>
2024-08-22 16:58:58 +0300

    asset images

---
+++ frontEnd/src/assets/assets.js
@@ -0,0 +1,18 @@
+import  add from './add.png'
+import brightness from './brightness.png'
+import product from './product.png'
+import watch from './watch.png'
+import delet from './delet.png'
+import  edit from './edit.png'
+
+
+const assets={
+    add,
+    brightness,
+    product,
+    watch,
+    delet,
+    edit
+  
+}
+export default assets
