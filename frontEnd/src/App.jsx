6bd38075b33f803a2caab1669a7f56934dc62d7d
niceman1234man <yihunietarekegn18@gmail.com>
2024-08-22 17:00:11 +0300

    App

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Create from "./components/Create";
+import UpdateProduct from "./components/UpdateProduct";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/update/:id" element={<UpdateProduct />} />
+      </Routes>
+    </>
+  );
+}
+
+
+export default App;
