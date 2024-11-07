76a6a6d318763a5a4f61120b89f22626d0bbe5f9
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-07 14:51:35 -0800

    signup

--- frontEnd/src/components/SignUp.jsx
+++ frontEnd/src/components/SignUp.jsx
@@ -1,27 +1,62 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler=()=>{
+    axios.post("",{
+        userName,
+        email,
+        password,
+    })
+  }
+
   return (
-    <div className='bg-gray-950 text-sky-600  h-screen'>
-        <div className=' flex flex-col items-center justify-center bg-slate-400 p-4 mx-auto w-[500px] font-bold rounded-lg shadow-2xl'>
-            <h2 className='text-2xl'>Sign Up</h2>
-            <form className='flex flex-col justify-center'>
-                <label htmlFor="fname" className='p-2'>First Name</label>
-                <input type="text"  placeholder='first Name' className='p-2 w-[400px] outline-green-400' />
-                <label htmlFor="lname" className='p-2'>Last Name</label>
-                <input type="text"  placeholder='Last Name' className='p-2 outline-green-400'/>
-                <label htmlFor="uname" className='p-2'>User Name</label>
-                <input type="text"  placeholder='User Name' className='p-2 outline-green-400' />
-                <label htmlFor="email" className='p-2'>Email</label>
-                <input type="email"  placeholder='Email' className='p-2 outline-green-400'/>
-                <label htmlFor="password" className='p-2'>Password</label>
-                <input type="password" placeholder='*******' className='p-2 outline-green-400' />
-                <button type='submit' className='p-2 bg-sky-200 m-2 rounded-lg'>Sign Up</button>
-            </form>
-            <p>Already Have An Account ? Login</p>
-        </div>
+    <div className="bg-gray-950 text-sky-600  h-screen">
+      <div className=" flex flex-col items-center justify-center bg-slate-400 p-4 mx-auto w-[500px] font-bold rounded-md shadow-2xl">
+        <h2 className="text-2xl">Sign Up</h2>
+        <form className="flex flex-col justify-center">
+          <label htmlFor="uname" className="p-2">
+            User Name
+          </label>
+          <input
+          onChange={(e)=>setUserName(e.target.value)}
+            type="text"
+            placeholder="User Name"
+            className="p-2 outline-green-400 w-[400px]"
+          />
+          <label htmlFor="email" className="p-2">
+            Email
+          </label>
+          <input
+          onChange={(e)=>setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+            className="p-2 outline-green-400 w-[400px]"
+          />
+          <label htmlFor="password" className="p-2">
+            Password
+          </label>
+          <input
+          onChange={(e)=>setPassword(e.target.value)}
+            type="password"
+            placeholder="*******"
+            className="p-2 outline-green-400 w-[400px]"
+          />
+          <button type="submit" className="p-2 bg-sky-200 m-2 rounded-lg">
+            Sign Up
+          </button>
+        </form>
+        <p>
+          Already Have An Account ? <Link to="/login">Login</Link>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
