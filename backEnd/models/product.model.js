ec695a55b04d5a81c5200205a4e91b0d116d62f3
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-07 22:36:58 -0800

    model

--- backEnd/models/product.model.js
+++ backEnd/models/product.model.js
@@ -1,5 +1,11 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 const productSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User",
+
+    },
 name:{
     type:String,
     required:true
