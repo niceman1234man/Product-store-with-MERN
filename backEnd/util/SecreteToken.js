6adc0bb43e110be9a26daf63973497dd35eb238c
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-07 16:10:58 -0800

    util

---
+++ backEnd/util/SecreteToken.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config();
+export const CreateSecreteToken=(id)=>{
+return jwt.sign({id},process.env.TOKEN_KEY,{
+    expiresIn:3*24*60*60,
+});
+}
