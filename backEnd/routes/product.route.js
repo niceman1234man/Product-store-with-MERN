c6df327c6fd23f746584817fa3520cc976e162d4
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-07 21:54:48 -0800

    router

--- backEnd/routes/product.route.js
+++ backEnd/routes/product.route.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { getProduct, getProductById,updateproduct,createProduct,deleteProduct} from '../controlers/product.control.js';
 import { Signup,Login } from '../controlers/userController.js';
+import { userVerify } from '../Authmiddleware/authmidlewar.js';
 
 const router=express.Router();
 
@@ -11,6 +12,7 @@ router.get("/:id",getProductById)
  router.delete("/:id",deleteProduct);
  router.post('/signup',Signup);
  router.post('/login',Login);
+ router.post('/',userVerify);
 
 
 
