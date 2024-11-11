62414e581edeeb892aec6410799f133ccaf917fe
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-10 22:03:42 -0800

    home

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -1,53 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import assets from "../assets/assets";
 import { useCookies } from "react-cookie";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import UserContext from "../Context/UserContext";
 
-import  axios from "axios";
 
 function Home() {
-
-  const [cookies,removeCookie]=useCookies([]);
-
-  const [products, setProducts] = useState([]);
-  
+  const [cookies, removeCookie] = useCookies([]);
+  const { user, setUser } =useContext(UserContext);// Access user from context
+  const [products, setProductsLocal] = useState([]);
   const navigate = useNavigate();
 
-
-  
   useEffect(() => {
-    if(!cookies.token){
-      navigate('/login');
-     }
-    axios
-      .get("http://localhost:3000/products",{withCredentials:true,})
-      .then((result) => {
-        console.log("API Result:", result);
-        setProducts(result.data.data);
-      })
-      .catch((err) => console.log("Error fetching products:", err));
-  }, [cookies, navigate, removeCookie]);
-  const onDeleteHandler = (id) => {
-    if (confirm("Are you Sure delete This Product")) {
+    if (!cookies.token) {
+      navigate("/login");
+    } else if (user) {
+      // Fetch products using the user's ID
       axios
-        .delete("http://localhost:3000/products/" + id)
-        .then((res) => {
-          console.log(res);
-          window.location.reload();
+        .get(`http://localhost:3000/products/user/${user._id}`, { withCredentials: true })
+        .then((result) => {
+          console.log("API Result:", result);
+          setProductsLocal(result.data.data); // Set products to local state
+        })
+        .catch((err) => console.log("Error fetching products:", err));
+    }
+  }, [cookies, navigate, user]);
+
+  const onDeleteHandler = (id) => {
+    if (confirm("Are you sure you want to delete this product?")) {
+      axios
+        .delete(`http://localhost:3000/products/${id}`, { withCredentials: true })
+        .then(() => {
+          setProductsLocal(products.filter((product) => product._id !== id)); // Update local state
         })
         .catch((err) => console.log(err));
     }
   };
+
   const Logout = () => {
     removeCookie("token");
     navigate("/signup");
-  }
+  };
+
   return (
-    <div
-      className={`h-fit bg-gray-950 grid md:grid-cols-2 lg:grid-cols-3 p-4 mx-auto`}
-      
-    >
-      <button onClick={Logout}>logout</button>
+    <div className={`h-fit bg-gray-950 grid md:grid-cols-2 lg:grid-cols-3 p-4 mx-auto`}>
+      <button onClick={Logout}>Logout</button>
       {products.length > 0 ? (
         products.map((product) => (
           <div
@@ -67,20 +65,14 @@ function Home() {
               <Link to={`/update/${product._id}`}>
                 <img src={assets.edit} alt="edit" className="w-5" />
               </Link>
-              <button
-                onClick={(e) => {
-                  onDeleteHandler(product._id);
-                }}
-              >
+              <button onClick={() => onDeleteHandler(product._id)}>
                 <img src={assets.delet} alt="delete" className="w-5" />
               </button>
             </div>
           </div>
         ))
       ) : (
-        <h1 className="text-white text-center text-2xl">
-          No products available
-        </h1>
+        <h1 className="text-white text-center text-2xl">No products available</h1>
       )}
     </div>
   );
