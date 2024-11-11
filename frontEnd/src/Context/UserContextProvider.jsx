ba18415d0c587d67c0b43bda8f99e6f00a872df5
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-10 22:03:01 -0800

    context provider

--- frontEnd/src/Context/UserContextProvider.jsx
+++ frontEnd/src/Context/UserContextProvider.jsx
@@ -1,13 +1,14 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import UserContext from './UserContext';
 
-const UserContextProvider=(children)=> {
-    const [user,setUser]=useState(null);
-  return (
-   < UserContext.Provider value={{user,setUser}}>
-   {children}
-   </UserContext.Provider>
-  )
+const UserContextProvider = ({ children }) => { // Destructure children from props
+    const [user, setUser] = useState(null);
+    
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children} {/* Render children here */}
+        </UserContext.Provider>
+    );
 }
 
-export default UserContextProvider
+export default UserContextProvider;
