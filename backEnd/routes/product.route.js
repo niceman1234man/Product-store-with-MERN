2034e5f2aff8664e64231f8187e67b2a1d6c6e07
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-11 22:08:53 -0800

    routes

--- backEnd/routes/product.route.js
+++ backEnd/routes/product.route.js
@@ -2,12 +2,13 @@ import express from 'express'
 import { getProduct, getProductById,updateproduct,createProduct,deleteProduct} from '../controlers/product.control.js';
 import { Signup,Login } from '../controlers/userController.js';
 import { userVerify } from '../Authmiddleware/authmidlewar.js';
+import auth from '../Auth/auth.js';
 
 const router=express.Router();
 router.get("/user/:userId",getProduct);
 router.get("/:id",getProductById)
  router.put("/:id",updateproduct );
- router.post("/",createProduct);
+ router.post("/",auth,createProduct);
  router.delete("/:id",deleteProduct);
  router.post('/signup',Signup);
  router.post('/login',Login);
