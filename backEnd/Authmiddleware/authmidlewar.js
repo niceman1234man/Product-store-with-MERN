7869d504a81f03cb2b9657c6016a0664a8345d29
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-13 13:08:27 -0800

    authmiddleware

--- backEnd/Authmiddleware/authmidlewar.js
+++ backEnd/Authmiddleware/authmidlewar.js
@@ -3,20 +3,17 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 dotenv.config();
 export const userVerify=(req,res,next)=>{
-const authHeader=req.headers['authorization'];
+const token=req.cookies.token;
 
-const token=authHeader&&authHeader.split(" ")[1];
 if(!token){
   return  res.status(401).json({
         succuss:false,message:"access denied. No token provided"
     })
 
-
-
 }
 try {
     const decodedTokenInfo=jwt.verify(token,process.env.TOKEN_KEY);
-    console.log(decodedTokenInfo);
+  
     req.userInfo=decodedTokenInfo;
     next();
  } catch (error) {
