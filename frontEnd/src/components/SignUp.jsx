981743bdb0e4b6e058e662b02ab1df25823a9339
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-13 17:09:35 -0800

    signup

--- frontEnd/src/components/SignUp.jsx
+++ frontEnd/src/components/SignUp.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 function SignUp() {
   const [username, setUserName] = useState("");
   const [email, setEmail] = useState("");
