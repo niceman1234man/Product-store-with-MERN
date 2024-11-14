c5466736046f4f279ccc8ce5222700441fbd3042
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-14 15:15:47 -0800

    login

--- frontEnd/src/components/Login.jsx
+++ frontEnd/src/components/Login.jsx
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { useColor } from "../Context/ColorContextProvider";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+    const {color}=useColor();
   const onSubmitHandler = (e) => {
     e.preventDefault();
     
@@ -32,12 +32,12 @@ function Login() {
   };
 
   return (
-    <div className="bg-gray-950 text-sky-600 h-screen">
+    <div className={`${color} text-sky-600 h-screen`}>
       <ToastContainer />
-      <div className="flex flex-col items-center justify-center mx-auto w-[500px] bg-slate-400 p-4 rounded-md">
+      <div className="flex flex-col items-center justify-center mx-auto w-[500px] bg-slate-400 p-4  ">
         <h2 className="text-2xl">LOGIN</h2>
         <form className="flex flex-col justify-center" onSubmit={onSubmitHandler}>
-          <label htmlFor="email" className="p-2">Email</label>
+          <label htmlFor="email" className="p-2 text-2xl">Email</label>
           <input
             id="email"
             onChange={(e) => setEmail(e.target.value)}
@@ -45,7 +45,7 @@ function Login() {
             placeholder="Email"
             className="p-2 w-[400px] outline-green-400"
           />
-          <label htmlFor="password" className="p-2">Password</label>
+          <label htmlFor="password" className="p-2 text-2xl">Password</label>
           <input
             id="password"
             onChange={(e) => setPassword(e.target.value)}
@@ -53,10 +53,10 @@ function Login() {
             placeholder="*******"
             className="p-2 w-[400px] outline-green-400"
           />
-          <button type="submit" className="p-2 bg-sky-200 m-2 rounded-lg">Login</button>
+          <button type="submit" className="p-2 bg-green-500 my-4 rounded-lg w-[30%] mx-auto font-semibold text-black">Login</button>
         </form>
-        <p>
-          Don't have an account? <Link to="/signup">Sign Up</Link>
+        <p className="text-xl">
+          Don't have an account? <Link to="/signup" className="text-violet-950">Sign Up</Link>
         </p>
       </div>
     </div>
