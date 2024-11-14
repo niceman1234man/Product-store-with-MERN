a26e604ae43eabd097c04dcbbc6f191071519865
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-14 15:14:17 -0800

    create

--- frontEnd/src/components/Create.jsx
+++ frontEnd/src/components/Create.jsx
@@ -3,10 +3,11 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useColor } from "../Context/ColorContextProvider";
 
 function Create() {
   const navigate = useNavigate();
-
+  const {color}=useColor();
   const [productName, setProductName] = useState("");
   const [productPrice, setProductPrice] = useState("");
   const [productURL, setProductURL] = useState("");
@@ -40,27 +41,28 @@ function Create() {
   };
 
   return (
-    <div className="bg-gray-950 h-screen flex flex-col items-center justify-center">
+    <div className={`${color} h-screen flex flex-col items-center justify-center`}>
       <ToastContainer position="top-center" autoClose={3000}/>
-      <h2 className="text-sky-500 py-2 text-2xl">Create New Product</h2>
-      <div className="w-[30%] flex items-center justify-center mx-auto">
+    
+      <div className="w-[30%] flex items-center justify-center mx-auto ">
         <form
           onSubmit={onSubmitHandler}
-          className="text-black flex flex-col justify-center py-10 my-4 mx-auto bg-slate-600 p-4 w-full max-w-md rounded-lg"
+          className="text-black flex flex-col justify-center p-10  mx-auto bg-slate-600  w-full max-w-md "
         >
+            <h2 className="text-sky-500 py-2 text-2xl mx-auto">Create New Product</h2>
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
           <input
             onChange={(e) => setProductName(e.target.value)}
             type="text"
             placeholder="Product Name"
-            className="p-2 m-2 rounded-md outline-sky-600"
+            className="p-2 m-2 outline-sky-600"
             required
           />
           <input
             onChange={(e) => setProductPrice(e.target.value)}
             type="number"
             placeholder="Product Price"
-            className="p-2 m-2 rounded-md outline-sky-600"
+            className="p-2 m-2 outline-sky-600"
             min="0"
             required
           />
@@ -68,10 +70,10 @@ function Create() {
             onChange={(e) => setProductURL(e.target.value)}
             type="text"
             placeholder="Image URL"
-            className="p-2 m-2 rounded-md outline-sky-600"
+            className="p-2 m-2  outline-sky-600"
             required
           />
-          <button type="submit" className="p-2 m-2 rounded-md bg-sky-900">
+          <button type="submit" className="p-2 m-2 rounded-md bg-sky-900 w-[40%] mx-auto">
             Create Product
           </button>
         </form>
