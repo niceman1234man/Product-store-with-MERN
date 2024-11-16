f3a507c3eb82adf96ed2f79479044d3ce5f23f0f
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-16 12:15:24 -0800

    navbar

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -2,22 +2,27 @@ import React, { useState} from "react";
 import { useNavigate } from "react-router-dom";
 import assets from "../assets/assets";
 import { useColor } from "../Context/ColorContextProvider";
-
+import { useCookies } from "react-cookie";
 function Navbar() {
   const navigate = useNavigate();
   const {color,toggleColor}=useColor();
+  const [cookies, removeCookie] = useCookies([]);
+  const handleLogout = () => {
+    removeCookie("token");
+    navigate("/login"); // Redirect to login page
+  };
 
   return (
     <div className={`fixed ${color} text-sky-400 px-auto w-[1240px]  py-4 h-[10%]`}>
       <div className="flex items-center justify-between mx-60">
         <h2
           className="flex gap-2 items-center justify-center py-4 font-bold cursor-pointer"
-          onClick={() => navigate("/")}
+      
         >
           PRODUCT STORE
           <img src={assets.product} alt="product logo" className="w-6" />
         </h2>
-        <div className="flex gap-1">
+        <div className="flex justify-center items-center">
           <img
             src={assets.add}
             alt="add"
@@ -28,8 +33,12 @@ function Navbar() {
             src={assets.brightness}
             alt="brightness"
             onClick={toggleColor}
-            className="bg-slate-400 w-5 cursor-pointer"
+            className="bg-slate-400 w-5 cursor-pointer mx-2"
           />
+          <button onClick={handleLogout} className="text-white p-1 m-4 bg-red-500 rounded ">
+        Logout
+      </button>
+     
         </div>
       </div>
     </div>
