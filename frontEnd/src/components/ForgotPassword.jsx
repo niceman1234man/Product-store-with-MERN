abd044d52dd35eb4700de7cd9178ea6dc2fce778
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-18 10:15:49 -0800

    forgot pasword

--- frontEnd/src/components/ForgotPassword.jsx
+++ frontEnd/src/components/ForgotPassword.jsx
@@ -28,21 +28,24 @@ function ForgotPassword() {
 
   return (
     <div className={`${color} text-sky-600 h-[90vh] flex justify-center items-center`}>
-      <div className="flex flex-col items-center justify-center mx-auto w-[500px] bg-slate-400 p-4">
-        <h2 className="text-2xl">Forgot Password</h2>
-        <form className="flex flex-col justify-center" onSubmit={onSubmitHandler}>
-          <label htmlFor="email" className="p-2 text-2xl">Email</label>
+      <div className="flex flex-col items-center justify-center mx-auto w-[50%] bg-slate-400 p-4">
+        <h2 className="text-sm md:text-xl lg:text-2xl">Forgot Password</h2>
+        <div className="bg-cyan-400 flex justify-start mx-auto p-2 rounded">
+        <button onClick={() => navigate(-1)}>Back</button>
+      </div>
+        <form className="flex flex-col justify-center w-[50%] " onSubmit={onSubmitHandler}>
+          <label htmlFor="email" className="p-2 text-sm md:text-xl ">Email</label>
           <input
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Email"
-            className="p-2 w-[400px] outline-green-400"
+            className="p-2 w-[90%] outline-green-400"
           />
           <button
             type="submit"
-            className="p-2 bg-green-500 my-4 rounded-lg w-[30%] mx-auto font-semibold text-black"
+            className="p-2 bg-green-500 my-4 rounded-lg w-[50%] mx-auto font-semibold text-black text-sm md:text-xl "
           >
             Send
           </button>
