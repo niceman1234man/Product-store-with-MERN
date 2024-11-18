065e2f6e0ef8f5907826dbcb90a587d67986a4c4
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-18 10:12:47 -0800

    nav bar

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { useColor } from "../Context/ColorContextProvider";
 function Navbar() {
   const { color, toggleColor } = useColor();
   return (
-    <div className={`fixed ${color} text-sky-400 px-auto w-full py-4 h-[10%]`}>
-      <div className="flex items-center justify-between mx-60">
-        <h2 className="flex gap-2 items-center justify-center py-4 font-bold cursor-pointer">
+    <div className={`fixed ${color} text-sky-400 px-auto w-full py-4 h-[10%] mx-auto`}>
+      <div className="flex items-center justify-center  mx-auto  ">
+        <h2 className="flex gap-2 items-center justify-center py-4 font-bold cursor-pointer mr-[5%] text-sm md:text-xl lg:text-2xl">
           PRODUCT STORE
           <img src={assets.product} alt="product logo" className="w-6" />
         </h2>
