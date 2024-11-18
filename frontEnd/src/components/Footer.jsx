a692b87e5090c36c98fbb4379ea146441d6cae18
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-17 16:09:18 -0800

    footer

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { useColor } from "../Context/ColorContextProvider";
 function Footer() {
+  const {color}=useColor();
     const year=new Date();
 const currrentyear=year.getFullYear();
   return (
     
-    <div className=' fixed h-[10%] w-full bg-black text-white py-2'>
+    <div className={ `${color} fixed h-[10%] w-full text-green-700 py-2`}>
         <div className='max-w-[1240px] text-center '>
         <p>{currrentyear} &copy; Yihunie T.</p>
         </div>
