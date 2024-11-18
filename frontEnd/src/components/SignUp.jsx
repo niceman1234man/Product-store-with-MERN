3f024f403562798d7018faa3ce8bf03027f8940d
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-18 10:12:10 -0800

    sign up

--- frontEnd/src/components/SignUp.jsx
+++ frontEnd/src/components/SignUp.jsx
@@ -24,11 +24,11 @@ function SignUp() {
   };
 
   return (
-    <div className={`${color} text-sky-600 h-[90vh] flex justify-center items-center`}>
-      <div className="flex flex-col items-center justify-center bg-slate-400 p-4 mx-auto w-[500px] font-bold shadow-2xl">
-        <h2 className="text-2xl">Sign Up</h2>
-        <form className="flex flex-col justify-center" onSubmit={onSubmitHandler}>
-          <label htmlFor="username" className="p-2">
+    <div className={`${color} text-sky-600 h-[90vh] flex justify-center items-center p-2`}>
+      <div className="flex flex-col items-center justify-center bg-slate-400  mx-auto w-[50%] font-bold shadow-2xl mt-8 py-4">
+        <h2 className="text-sm md:text-xl ">Sign Up</h2>
+        <form className="flex flex-col justify-center w-[80%] " onSubmit={onSubmitHandler}>
+          <label htmlFor="username" className="p-2 text-sm md:text-xl">
             User Name
           </label>
           <input
@@ -36,9 +36,9 @@ function SignUp() {
             onChange={(e) => setUserName(e.target.value)}
             type="text"
             placeholder="User Name"
-            className="p-2 outline-green-400 w-[400px]"
+            className="p-2 outline-green-400 w-[90%]"
           />
-          <label htmlFor="email" className="p-2">
+          <label htmlFor="email" className="p-2 text-sm md:text-xl">
             Email
           </label>
           <input
@@ -46,9 +46,9 @@ function SignUp() {
             onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Email"
-            className="p-2 outline-green-400 w-[400px]"
+            className="p-2 outline-green-400 w-[90%]"
           />
-          <label htmlFor="password" className="p-2">
+          <label htmlFor="password" className="p-2 text-sm md:text-xl">
             Password
           </label>
           <input
@@ -56,13 +56,13 @@ function SignUp() {
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="*******"
-            className="p-2 outline-green-400 w-[400px]"
+            className="p-2 outline-green-400 w-[90%]"
           />
-          <button type="submit" className="p-2 bg-sky-200 my-4 rounded-lg w-[30%] mx-auto">
+          <button type="submit" className="p-2 bg-sky-200 my-4 rounded-lg w-[30%] mx-auto text-sm md:text-xl">
             Sign Up
           </button>
         </form>
-        <p>
+        <p className="text-sm md:text-xl">
           Already Have An Account? <Link to="/" className="text-purple-900">Login</Link>
         </p>
       </div>
