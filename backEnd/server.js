2272df8ec77d7ad7ca2f63cff50c247519112182
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-17 16:30:19 -0800

    server

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,8 +3,24 @@ import { connectDb } from './config/db.js';
 import cookieParser from 'cookie-parser';
 import productRoutes from './routes/product.route.js'
 import cors from 'cors'
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files
+
+
 const app=express();
 app.use(cookieParser());
+
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
 app.use(cors({
     origin:"http://localhost:5173",
     methods:["GET","PUT","DELETE","POST"],
