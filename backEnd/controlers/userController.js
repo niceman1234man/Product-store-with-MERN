0139575ba5368ea666226cb1ab8223bda9ccef28
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-17 16:29:14 -0800

    user controller

--- backEnd/controlers/userController.js
+++ backEnd/controlers/userController.js
@@ -3,6 +3,7 @@ import User from "../models/userModel.js";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer'
 dotenv.config();
 
 export const Signup = async (req, res) => {
@@ -71,9 +72,79 @@ export const Login = async (req, res) => {
            
         });
 
+        
         res.status(200).json({ success: true, message: "User logged in successfully", accessToken });
     } catch (error) {
         console.error("Login error:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const ForgotPassword=async(req,res)=>{
+const {email}=req.body;
+try {
+   const user=await User.findOne(email) ;
+   if(!user){
+    return res.status(401).json({sucess:true,message:"user not exist try again"})
+   }
+   const accessToken = jwt.sign(
+    { userId: user._id, },
+    process.env.TOKEN_KEY,
+    { expiresIn: "3d" }
+);
+
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+var mailOptions = {
+  from: process.env.USER_EMAIL,
+  to: `${user.email}`,
+  subject: 'Reset Your Password',
+  text:`http://localhost:5173//reset/${user._id}/${accessToken}`
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    res.json({sucess:true})
+  }
+});
+} catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Internal server error" });
+}
+
+}
+
+export const ResetPassword=(req,res)=>{
+    const {id,token}=req.params;
+    const {password}=req.body;
+    jwt.verify(token, process.env.TOKEN_KEY,(err,decoded)=>{
+        if(err){
+            return res.json({sucess:false,message:"Error token"});
+        }else{
+            try {
+    
+                const hashedPassword=bcrypt.hash(password,12);
+                User.findByIdAndUpdate({_id:userId,password:hashedPassword}).then(()=>{
+                    res.status(200).json({sucess:true,message:"Password reseted sucessfully"})
+                }).catch((err)=>{
+            console.log(err);
+                })
+            } catch (error) {
+                console.log(error)
+                res.status(500).json({ message: "Internal server error" });   
+            }
+              
+        }
+    });
+
+}
