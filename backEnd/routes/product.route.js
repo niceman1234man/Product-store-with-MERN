be09dbf3d4c4193f2c9e34cf3406b0422a4c0d5d
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-17 16:29:46 -0800

    router

--- backEnd/routes/product.route.js
+++ backEnd/routes/product.route.js
@@ -1,18 +1,20 @@
 import express from 'express'
 import { getProduct, getProductById,updateproduct,createProduct,deleteProduct} from '../controlers/product.control.js';
-import { Signup,Login } from '../controlers/userController.js';
+import { Signup,Login, ForgotPassword, ResetPassword } from '../controlers/userController.js';
 import { userVerify } from '../Authmiddleware/authmidlewar.js';
+import { upload } from '../fileMiddleware.js';
 
 
 const router=express.Router();
 router.get("/user",userVerify,getProduct);
-router.get("/:id",getProductById)
- router.put("/:id",updateproduct );
- router.post("/create",userVerify,createProduct);
- router.delete("/:id",deleteProduct);
+router.get("/:id",userVerify,getProductById)
+ router.put("/:id",upload.single("image"),userVerify,updateproduct );
+ router.post("/create",upload.single("image"),userVerify,createProduct);
+ router.delete("/:id",userVerify,deleteProduct);
  router.post('/signup',Signup);
  router.post('/login',Login);
-//  router.post('/v',userVerify);
+router.post('/forget',ForgotPassword);
+router.post('/reset/:user/:token',ResetPassword);
 
 
 
